c7151494272de41aec7e5c13ff5b861a896dd3a9
walljs <walljs@163.com>
2021-02-04 00:13:27 +0800

    edit merge

--- merge/merge.js
+++ merge/merge.js
@@ -5,22 +5,26 @@ function merge(nums1, m, nums2, n) {
     var p2 = 0;
     var p = 0;
     while ((p1 < m) && (p2 < n)) {
-        nums1[p++] = (nums1Copy[p1] < nums2[p2]) ? nums1Copy[p1++] : nums2[p2++];
+        if (nums1Copy[p1] < nums2[p2]) {
+            nums1[p++] = nums1Copy[p1++];
+        }
+        else {
+            nums1[p++] = nums2[p2++];
+        }
     }
     while (p1 < m) {
-        nums1[p1 + p2] = nums1Copy[p1];
-        p1++;
+        console.log(111);
+        nums1[p++] = nums1Copy[p1++];
     }
     while (p2 < n) {
-        nums1[p1 + p2] = nums2[p2];
-        p2++;
+        nums1[p++] = nums2[p2++];
     }
 }
 ;
-// const num1: number[] = [1,2,3,0,0,0];
-// const num2: number[] = [2,5,6];
-// merge(num1, 3, num2, 3);
-var num1 = [0];
-var num2 = [1];
-merge(num1, 0, num2, 1);
+var num1 = [1, 2, 3, 0, 0, 0];
+var num2 = [2, 5, 6];
+merge(num1, 3, num2, 3);
+// const num1 = [0]
+// const num2 = [1]
+// merge(num1, 0, num2, 1);
 console.log(num1);
--- merge/merge.ts
+++ merge/merge.ts
@@ -6,23 +6,26 @@ function merge(nums1: number[], m: number, nums2: number[], n: number): void {
   let p: number = 0;
 
   while ((p1 < m) && (p2 < n)) {
-    nums1[p++] = (nums1Copy[p1] < nums2[p2]) ? nums1Copy[p1++] : nums2[p2++];
+    if (nums1Copy[p1] < nums2[p2]) {
+      nums1[p++] = nums1Copy[p1++];
+    } else {
+      nums1[p++] = nums2[p2++];
+    }
   }
 
   while (p1 < m) {
-    nums1[p1 + p2] = nums1Copy[p1];
-    p1++;
+    console.log(111);
+    nums1[p++] = nums1Copy[p1++];
   }
   while (p2 < n) {
-    nums1[p1 + p2] = nums2[p2];
-    p2++;
+    nums1[p++] = nums2[p2++];
   }
 };
 
-// const num1: number[] = [1,2,3,0,0,0];
-// const num2: number[] = [2,5,6];
-// merge(num1, 3, num2, 3);
-const num1 = [0]
-const num2 = [1]
-merge(num1, 0, num2, 1);
+const num1: number[] = [1,2,3,0,0,0];
+const num2: number[] = [2,5,6];
+merge(num1, 3, num2, 3);
+// const num1 = [0]
+// const num2 = [1]
+// merge(num1, 0, num2, 1);
 console.log(num1);
