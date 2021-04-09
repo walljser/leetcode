219b47aed192d5ae2b1f0e5488381edf4b13b8be
wall <walljs@163.com>
2021-04-10 00:12:09 +0800

    509.fib

--- first/509fib/fib.js
+++ first/509fib/fib.js
@@ -21,26 +21,31 @@
 //   return dp[n];
 // }
 
+// var fib = function (n) {
+//   let pre1 = 0;
+//   let pre2 = 0;
+//   let res = 0;
+//   for (let i = 0; i <= n; i++) {
+//     if (i === 0) {
+//       res = 0;
+//       pre1 = 0;
+//       pre2 = 0;
+//     } else if (i === 1) {
+//       res = 1;
+//       pre1 = 1;
+//       pre2 = 0;
+//     } else {
+//       res = pre1 + pre2;
+//       pre2 = pre1;
+//       pre1 = res;
+//     }
+//   }
+//   return res;
+// }
+
 var fib = function (n) {
-  let pre1 = 0;
-  let pre2 = 0;
-  let res = 0;
-  for (let i = 0; i < n; i++) {
-    if (i === 0) {
-      res = 0;
-      pre1 = 0;
-      pre2 = 0;
-    } else if (i === 1) {
-      res = 1;
-      pre1 = 1;
-      pre2 = 0;
-    } else {
-      res = pre1 + pre2;
-      pre2 = pre1;
-      pre1 = res;
-    }
-  }
-  return res;
+  const  table = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040];
+  return table[n];
 }
 
 console.log(fib(9));
