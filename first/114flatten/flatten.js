c3bf4300d5a01e1e05f0189e482691e1333386b4
wall <walljs@163.com>
2021-04-13 22:50:19 +0800

    114.flatten

--- first/114flatten/flatten.js
+++ first/114flatten/flatten.js
@@ -2,24 +2,44 @@
  * @param {TreeNode} root
  * @return {void} Do not return anything, modify root in-place instead.
  */
+// var flatten = function (root) {
+//   if (!root) {
+//     return;
+//   }
+//   const list = [];
+//   const preTraverse = function (root) {
+//     if (!root) {
+//       list.push(root);
+//       preTraverse(root.left);
+//       preTraverse(root.right);
+//     }
+//   }
+//   preTraverse(root);
+//   const size = list.length;
+//   for (let i = 1; i < size; i++) {
+//     const prev = list[i - 1];
+//     const current = list[i];
+//     prev.left = null;
+//     prev.next = current;
+//   }
+// };
+
 var flatten = function (root) {
   if (!root) {
     return;
   }
-  const list = [];
-  const preTraverse = function (root) {
-    if (!root) {
-      list.push(root);
-      preTraverse(root.left);
-      preTraverse(root.right);
-    }
+  flatten(root.left);
+  flatten(root.right);
+
+  const left = root.left;
+  const right = root.right;
+
+  root.left = null;
+  root.right = left;
+
+  let p = root;
+  while (p.right) {
+    p = p.right;
   }
-  preTraverse(root);
-  const size = list.length;
-  for (let i = 1; i < size; i++) {
-    const prev = list[i - 1];
-    const current = list[i];
-    prev.left = null;
-    prev.next = current;
-  }
-};
+  p.right = right;
+}
