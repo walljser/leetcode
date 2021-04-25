a23c4ebdba78b52af0b4b534197149e798e5900c
wall <walljs@163.com>
2021-04-25 22:50:52 +0800

    460.LFUCache

--- first/460LFUCache/LFUCache.js
+++ first/460LFUCache/LFUCache.js
@@ -1,3 +1,19 @@
+var Node = function(_key, _val) {
+  this.key = _key;
+  this.val = _val;
+  this.prev = null;
+  this.next = null;
+}
+
+var DoubleList = function() {
+  const head = new Node(0, 0);
+  const tail = new Node(0, 0);
+  head.next = tail;
+  tail.prev = head;
+  this.head = head;
+  this.tail = tail;
+}
+
 /**
  * @param {number} capacity
  */
@@ -14,7 +30,13 @@ var LFUCache = function(capacity) {
  * @return {number}
  */
 LFUCache.prototype.get = function(key) {
-
+  let node = this.keyToVal.get(key);
+  if (!node) {
+    return -1;
+  }
+  const value = this.keyToVal.get(key);
+  this.addFeq(key);
+  return value;
 };
 
 /**
@@ -23,28 +45,89 @@ LFUCache.prototype.get = function(key) {
  * @return {void}
  */
 LFUCache.prototype.put = function(key, value) {
+  if (this.keyToFeq.size >= this.max) {
+    const minFeqKeyMap = this.feqToKeys.get(this.minFeq);
+  } else {
 
+  }
 };
 
 /**
- * Your LFUCache object will be instantiated and called as such:
- * var obj = new LFUCache(capacity)
- * var param_1 = obj.get(key)
- * obj.put(key,value)
+ *
+ * @param {*} key
+ * @param {*} value
  */
-
-const LinkedHashSet = function() {
-  const head = new Node(0, 0);
-  const tail = new Node(0, 0);
-  head.next = tail;
-  tail.prev = head;
-  this.head = head;
-  this.tail = tail;
+LFUCache.prototype.deleteMinFeqKey = function(key, value) {
+  const minFeqKeyMap = this.feqToKeys.get(this.minFeq);
+  this.feqKeyMap
 }
 
-const Node = function(_key, _val) {
-  this.key = _key;
-  this.val = _val;
-  this.prev = null;
-  this.next = null;
+/**
+ * 增加使用频率
+ * @param {*} key
+ */
+LFUCache.prototype.addFeq = function(key) {
+  const feq = this.keyToFeq.get(key) || 0;
+  const newFeq = feq + 1;
+  this.deleteFromFeqToKeys(key);
+  this.addToFeqToKeys(key, newFeq);
+  this.keyToFeq.set(key, newFeq)
 }
+
+/**
+ * 向上找最小的使用频率，更新
+ * @param {*} feq
+ * @returns
+ */
+LFUCache.prototype.findMinFeq = function(feq) {
+  if (this.feqToKeys.size === 0) {
+    return;
+  }
+  const feqKeyMap = this.feqToKeys.get(feq);
+  if (feqKeyMap.size > 0) {
+    this.minFeq = feq;
+    return feq;
+  } else {
+    this.findMinFeq(feq + 1);
+  }
+}
+
+/**
+ * 从feqToKeys中删除一个key
+ * @param {*} key
+ */
+LFUCache.prototype.deleteFromFeqToKeys = function(key) {
+  const feq = this.keyToFeq.get(key);
+  const feqKeyMap = this.feqToKeys.get(feq);
+  if (this.minFeq === feq) {
+    // 属于最小使用频率圈
+    feqKeyMap.delete(key);
+    if (feqKeyMap.size === 0) {
+      this.feqToKeys.delete(feq);
+      this.findMinFeq(feq + 1);
+    }
+  } else {
+    feqKeyMap.delete(feq)
+    if (feqKeyMap.size === 0) {
+      this.feqToKeys.delete(feq);
+    }
+  }
+}
+
+/**
+ * 向feqToKeys中添加一个key
+ * @param {*} key
+ * @param {*} feq
+ */
+LFUCache.prototype.addToFeqToKeys = function(key, feq) {
+  let feqKeyMap = this.feqToKeys.get(feq);
+  if (feqKeyMap) {
+    feqKeyMap.set(key, true);
+  } else {
+    feqKeyMap = new Map();
+    feqKeyMap.set(key, true);
+    this.feqToKeys.set(feq, feqKeyMap);
+  }
+}
+
+const lf = new LFUCache();
